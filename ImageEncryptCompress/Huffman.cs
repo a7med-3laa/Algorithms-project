92bb3a072b6e8fa15c9764faec1aa5b23f40e644
AhmedHassaan <ahmedhassaan162@gmail.com>
2017-12-28 00:46:27 +0200

    comments fixd

--- ImageEncryptCompress/Huffman.cs
+++ ImageEncryptCompress/Huffman.cs
@@ -8,89 +8,89 @@ namespace ImageQuantization
 {
     class Huffman
     {
-        public Node start;                  //o(1)
-        PriorityQueue nodes;                //o(1)
-        PriorityQueue nodes2;               //o(1)
+        public Node start;                  //ϴ(1)
+        PriorityQueue nodes;                //ϴ(1)
+        PriorityQueue nodes2;               //ϴ(1)
 
         public Huffman(PriorityQueue n)
         {
-            nodes = n;                      //o(1)
-            start = null;                   //o(1)
-            nodes2 = new PriorityQueue(n);  //o(1)
-            constructTree();                //o(1)
+            nodes = n;                      //ϴ(1)
+            start = null;                   //ϴ(1)
+            nodes2 = new PriorityQueue(n);  //ϴ(1)
+            constructTree();                //ϴ(1)
 
         }
 
 
-        public List<Node> Colors = new List<Node>();            //o(1)
-        public Dictionary<byte, KeyValuePair<int, List<bool>>> ColorsMap = new Dictionary<byte, KeyValuePair<int, List<bool>>>();   //o(1)
-        public void getBinary(Node parent, string bin)          //o(n)
+        public List<Node> Colors = new List<Node>();            //ϴ(1)
+        public Dictionary<byte, KeyValuePair<int, List<bool>>> ColorsMap = new Dictionary<byte, KeyValuePair<int, List<bool>>>();   //ϴ(1)
+        public void getBinary(Node parent, string bin)          //ϴ(K * Z) where k is color count and Z is Binary Size
         {
-            if (parent.right != null)                           //o(n)                
+            if (parent.right != null)                           //ϴ(1)                
             {
-                getBinary(parent.right, bin + "1");             //o(n)
+                getBinary(parent.right, bin + "1");             //ϴ(1)
             }
-            if (!parent.hasChildreen())                         //o(n)
+            if (!parent.hasChildreen())                         //ϴ(1)
             {
-                List<bool> bb = new List<bool>();               //o(n)
-                char[] c = bin.ToCharArray();                   //o(n)
-                for (int i = 0; i < c.Length; i++)              //o(n)
-                    if (c[i] == '0')                            //o(n)
-                        bb.Add(false);                          //o(n)
+                List<bool> bb = new List<bool>();               //ϴ(1)
+                char[] c = bin.ToCharArray();                   //ϴ(1)
+                for (int i = 0; i < c.Length; i++)              //ϴ(Z)
+                    if (c[i] == '0')                            //ϴ(1)
+                        bb.Add(false);                          //ϴ(1)
                     else
-                        bb.Add(true);                           //o(n)
-                parent.Binary = bb;                             //o(n)
-                Colors.Add(parent);                             //o(n)
-                ColorsMap.Add(parent.color, new KeyValuePair<int, List<bool>>(parent.frequnecy, parent.Binary));    //o(n)
+                        bb.Add(true);                           //ϴ(1)
+                parent.Binary = bb;                             //ϴ(1)
+                Colors.Add(parent);                             //ϴ(1)
+                ColorsMap.Add(parent.color, new KeyValuePair<int, List<bool>>(parent.frequnecy, parent.Binary));    //ϴ(1)
             }
 
-            if (parent.left != null)                            //o(n)
+            if (parent.left != null)                            //ϴ(1)
             {
-                getBinary(parent.left, bin + "0");              //o(n)
+                getBinary(parent.left, bin + "0");              //ϴ(1)
             }
         }
 
-        private void constructTree()
+        private void constructTree()                             //ϴ(n log n)  where n is color size
         {
-            Node tmp = new Node();                              //o(log n)
-            while (nodes.Count() > 1)                           //o(log n)
+            Node tmp = new Node();                              //ϴ(1)
+            while (nodes.Count() > 1)                           //ϴ(n log n)
             {
-                Node right = nodes.Dequeue();                   //o(log n)
-                Node left = nodes.Dequeue();                    //o(log n)
-                tmp.left = left;                                //o(log n)
-                tmp.right = right;
-                tmp.frequnecy = left.frequnecy + right.frequnecy;   //o(log n)
-                tmp.color = (byte)tmp.frequnecy;                //o(log n)
-                nodes.Enqueue(tmp);                             //o(log n)
-                tmp = new Node();                               //o(log n)
+                Node right = nodes.Dequeue();                   //ϴ(1)
+                Node left = nodes.Dequeue();                    //ϴ(1)
+                tmp.left = left;                                //ϴ(1)
+                tmp.right = right;                            //ϴ(1)
+                tmp.frequnecy = left.frequnecy + right.frequnecy;   //ϴ(1)
+                tmp.color = (byte)tmp.frequnecy;                //ϴ(1)
+                nodes.Enqueue(tmp);                             //ϴ(log n)
+                tmp = new Node();                               //ϴ(1)
             }
 
 
-            start = nodes.Dequeue();                            //o(log n)
-            getBinary(start, "");                               //o(log n)
+            start = nodes.Dequeue();                            //ϴ(1)
+            getBinary(start, "");                               //ϴ(1)
         }
-        public String writeHuffman()
+        public String writeHuffman()                    //ϴ(K) where K is colour count
         {
-            string s = "";                                      //o(n)
-            List<Node> dos = nodes2.getList();                  //o(n)
-            for (int i = 0; i < dos.Count; i++)                 //o(n)
+            string s = "";                                      //ϴ(1)
+            List<Node> dos = nodes2.getList();                  //ϴ(1)
+            for (int i = 0; i < dos.Count; i++)                 //ϴ(K)
             {
-                Node n = dos[i];                                //o(n)
-                s += n.color;                                   //o(n)
-                s += ",";                                       //o(n)
-                s += n.frequnecy;                               //o(n)
-                s += ",";                                       //o(n)
+                Node n = dos[i];                                //ϴ(1)
+                s += n.color;                                   //ϴ(1)
+                s += ",";                                       //ϴ(1)
+                s += n.frequnecy;                               //ϴ(1)
+                s += ",";                                       //ϴ(1)
 
             }
-            return s;                                           //o(n)
+            return s;                                           //ϴ(1)
         }
 
-        public long getCompressedSize()
+        public long getCompressedSize()             //ϴ(n) where n is color count
         {
-            long ans = 0;                                       //o(1)
-            for (int i = 0; i < Colors.Count; i++)              //o(1)
-                ans += ColorsMap[Colors[i].color].Value.Count * ColorsMap[Colors[i].color].Key;           //o(1) 
-            return ans;                                         //o(1)
+            long ans = 0;                                       //ϴ(1)
+            for (int i = 0; i < Colors.Count; i++)              //ϴ(n)
+                ans += ColorsMap[Colors[i].color].Value.Count * ColorsMap[Colors[i].color].Key;           //ϴ(1) 
+            return ans;                                         //ϴ(1)
         }
     }
 }
--- ImageEncryptCompress/PriorityQueue.cs
+++ ImageEncryptCompress/PriorityQueue.cs
@@ -6,114 +6,108 @@ using System.Text;
 namespace ImageQuantization
 {   public class Node
     {
-        public Node left, right;            //o(1)
-        public int frequnecy;               //o(1)
-        public byte color;                  //o(1)
-        public List<bool> Binary;           //o(1)
+        public Node left, right;            //ϴ(1)
+        public int frequnecy;               //ϴ(1)
+        public byte color;                  //ϴ(1)
+        public List<bool> Binary;           //ϴ(1)
         public Node()
         {
-            left = null;                    //o(1)
-            right = null;                   //o(1)
-            frequnecy = 0;                  //o(1)
-            color = 0;                      //o(1)
-            Binary = new List<bool>();      //o(1)
+            left = null;                    //ϴ(1)
+            right = null;                   //ϴ(1)
+            frequnecy = 0;                  //ϴ(1)
+            color = 0;                      //ϴ(1)
+            Binary = new List<bool>();      //ϴ(1)
         }
 
         public Node(byte C,int F)
         {
-            left = null;                    //o(1)
-            right = null;                   //o(1)
-            frequnecy = F;                  //o(1)
-            color = C;                      //o(1)
-            Binary = new List<bool>();      //o(1)
+            left = null;                    //ϴ(1)
+            right = null;                   //ϴ(1)
+            frequnecy = F;                  //ϴ(1)
+            color = C;                      //ϴ(1)
+            Binary = new List<bool>();      //ϴ(1)
         }
         public bool hasChildreen()
         {
-            return !((left == null) && (right == null));    //o(1)
+            return !((left == null) && (right == null));    //ϴ(1)
         }
     }
 
 
     public class PriorityQueue
     {
-        private List<Node> data;                    //o(1)
+        private List<Node> data;                    //ϴ(1)
 
         public PriorityQueue()
         {
-            this.data = new List<Node>(256);        //o(1)
+            this.data = new List<Node>(256);        //ϴ(1)
         }
-        public PriorityQueue(PriorityQueue temp)
-         {
-             this.data = new List<Node>();          //o(1)
-             for(int i = 0; i<temp.data.Count; i++) //o(n)
-             {
-                 data.Add(temp.data[i]);            //o(n)
-             }
+        public PriorityQueue(PriorityQueue temp)  // ϴ(n) where n is color count
+        {
+             this.data = new List<Node>();          //ϴ(1)
+            for (int i = 0; i<temp.data.Count; i++) //ϴ(n)
+            {
+                 data.Add(temp.data[i]);            //ϴ(1)
+            }
          }
  
 
        
 
-        public void Enqueue(Node item)
+        public void Enqueue(Node item)      // ϴ(log n) where n is colors size
         {
-            data.Add(item);                         //o(1)
-            int ci = data.Count - 1;                //o(1)
-            while (ci > 0)                          //o(log n)
+            data.Add(item);                         //ϴ(1)
+            int ci = data.Count - 1;                //ϴ(1)
+            while (ci > 0)                          //ϴ(log n)
             {
-                int pi = (ci - 1) / 2;              //o(log n)
-                if (CompareTo(data[ci],data[pi]) >= 0) break;   //o(logn n)
-                Node temp = data[ci];               //o(log n)
-                data[ci] = data[pi];                //o(log n)
-                data[pi] = temp;                    //o(log n)
-                ci = pi;                            //o(log n)
+                int pi = (ci - 1) / 2;              //ϴ(1)
+                if (CompareTo(data[ci],data[pi]) >= 0) break;   //ϴ(1)
+                Node temp = data[ci];               //ϴ(1)
+                data[ci] = data[pi];                //ϴ(1)
+                data[pi] = temp;                    //ϴ(1)
+                ci = pi;                            //ϴ (1)
             }
         }
 
-        public Node Dequeue()
+        public Node Dequeue()           //ϴ(log n) where n is colour count
         {
-            int li = data.Count - 1;                //o(1)
-            Node frontItem = data[0];               //o(1)
-            data[0] = data[li];                     //o(1)
-            data.RemoveAt(li);                      //o(1)
+            int li = data.Count - 1;                //ϴ(1)
+            Node frontItem = data[0];               //ϴ(1)
+            data[0] = data[li];                     //ϴ(1)
+            data.RemoveAt(li);                      //ϴ(1)
 
-            --li;                                   //o(1)
-            int pi = 0;                             //o(1)
-            while (true)                            //o(log n)
+            --li;                                   //ϴ(1)
+            int pi = 0;                             //ϴ(1)
+            while (true)                            //ϴ(log n)
             {
-                int ci = pi * 2 + 1;                //o(log n)
-                if (ci > li) break;                 //o(log n)
-                int rc = ci + 1;                    //o(log n)
-                if (rc <= li && CompareTo(data[rc], data[ci]) < 0) //o(log n)
-                    ci = rc;                        //o(log n)
-                if (CompareTo(data[pi], data[ci]) <= 0) break;   //o(log n)
-                Node temp = data[ci];               //o(log n)
-                data[ci] = data[pi];                //o(log n)
-                data[pi] = temp;                    //o(log n)
-                pi = ci;                            //o(log n)
+                int ci = pi * 2 + 1;                //ϴ(1)
+                if (ci > li) break;                 //ϴ(1)
+                int rc = ci + 1;                    //ϴ(1)
+                if (rc <= li && CompareTo(data[rc], data[ci]) < 0) //ϴ(1)
+                    ci = rc;                        //ϴ(1)
+                if (CompareTo(data[pi], data[ci]) <= 0) break;   //ϴ(1)
+                Node temp = data[ci];               //ϴ(1)
+                data[ci] = data[pi];                //ϴ(1)
+                data[pi] = temp;                    //ϴ(1)
+                pi = ci;                            //ϴ(1)
             }
-            return frontItem;                       //o(1)
+            return frontItem;                       //ϴ(1)
         }
-
-        public Node Peek()
+        
+        public int Count()                              //ϴ(1)
         {
-            Node frontItem = data[0];               //o(1)
-            return frontItem;                       //o(1)
+            return data.Count;                      //ϴ(1)
         }
-
-        public int Count()
-        {
-            return data.Count;                      //o(1)
-        }
-        public List<Node> getList()
+        public List<Node> getList()                 // ϴ(1)
         {
 
-            return data;
+            return data;                            //ϴ(1)
         }
-        public int CompareTo(Node other1, Node other2)
+        public int CompareTo(Node other1, Node other2)                      //ϴ(1)
         {
-            if (other1.frequnecy < other2.frequnecy) return -1;     //o(1)
-            else if (other1.frequnecy > other2.frequnecy) return 1; //o(1)
-            else return 0;                                          //o(1)
+            if (other1.frequnecy < other2.frequnecy) return -1;             //ϴ(1)
+            else if (other1.frequnecy > other2.frequnecy) return 1;       //ϴ(1)
+            else return 0;                                          //ϴ(1)
         }
     }
 }
